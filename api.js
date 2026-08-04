301e55004bea1199ea396341bf0761eae79b42b8
zainmath212-del <zainmath212@gmail.com>
2026-08-04 23:25:35 +0700

    Implement attendance and student search functions
    Added functions to send attendance and search for students using the API.

--- api.js
+++ api.js
@@ -1,1 +1,26 @@
+const API_URL =
+"https://script.google.com/macros/s/AKfycbzxRG0MglizzdThP38rGaAmuIQcZeGM0Xtu-4fwbOBM2u4YVhhdqfVWonU8sdYJgAWUtQ/exec";
 
+async function sendAttendance(id){
+
+    const response = await fetch(
+        API_URL + "?id=" + encodeURIComponent(id)
+    );
+
+    return await response.json();
+
+}
+
+async function searchStudent(keyword){
+
+    const response = await fetch(
+
+        API_URL +
+        "?action=search&keyword=" +
+        encodeURIComponent(keyword)
+
+    );
+
+    return await response.json();
+
+}
